12d0137d7170cf7d069faa0521500ff0131e82aa
AlExito745 <alxtinoco1988@gmail.com>
2022-12-14 08:06:05 -0800

    prototypes

---
+++ shell.h
@@ -0,0 +1,24 @@
+#ifndef SHELL
+#define SHELL
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <signal.h>
+char *read_line(void);
+char **splits(char *line, char *delim);
+int execute(char **parse);
+
+
+
+char **find_path(char **environ);
+
+char *args_path(char **parse, char **tokens);
+
+
+
+#endif
